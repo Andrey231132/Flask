b3f4af1ad715e32805200b2d56fbb73285311756
Andrey231 <osad.andrey@yandex.ru>
2023-06-16 10:10:38 +0300

    Add html content

--- FlaskCode.py
+++ FlaskCode.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Это мой сайт'
+    return render_template("HTML_FILE.html")
 
